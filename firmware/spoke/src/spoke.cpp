cef81dd1febec0986922c590f0a62e921f61ded1
Thomas Rix <tom@rixth.org>
2014-08-10 16:21:42 -0400

    Diag test  code

--- firmware/spoke/src/spoke.cpp
+++ firmware/spoke/src/spoke.cpp
@@ -1,4 +1,3 @@
-#include <avr/io.h>
 #include <util/delay.h>
 
 #include <pins.h>
@@ -6,43 +5,93 @@
 #include <SerialIO.h>
 #include <EEPROM.h>
 #include <DHT11.h>
+#include <IRSend.h>
+
+#include <spoke.h>
 
 int main() {
   SerialIO_Init();
   SPI_begin();
   EEPROM_init();
-
-  bool redOn = false;
-  bool yellowOn = false;
-  bool greenOn = false;
+  IRSend_init();
 
   RED_LED_ToOutput();
   GRN_LED_ToOutput();
   YLW_LED_ToOutput();
 
   while (1) {
-    while (SerialIO_hasData()) {
-      uint8_t byt = SerialIO_recv();
-      if (byt == '1') redOn = !redOn;
-      if (byt == '2') yellowOn = !yellowOn;
-      if (byt == '3') greenOn = !greenOn;
-    }
-
-    uint8_t r = DHT11_readSensor();
-    SerialIO_puts("Read result: ");
-    SerialIO_putb(r);
-    SerialIO_puts(" T: ");
-    SerialIO_putb(DHT11_temperature());
-    SerialIO_puts("C H: ");
-    SerialIO_putb(DHT11_humidity());
-    SerialIO_puts("%\n");
-
-    _delay_ms(500);
-
-    redOn ? RED_LED_High() : RED_LED_Low();
-    yellowOn ? YLW_LED_High() : YLW_LED_Low();
-    greenOn ? GRN_LED_High() : GRN_LED_Low();
+    mainTest(SerialIO_recv());
   }
 
   return 0;
+}
+
+void testEEPROM(uint8_t input) {
+  SerialIO_puts("\n -- EEPROM TEST -- \n");
+
+  int addr = 0;
+  uint8_t data[3] = { input, input + 1, input + 2 };
+
+  SerialIO_puts("Writing 3 bytes: ");
+  SerialIO_putb(data[0]);
+  SerialIO_puts(" - ");
+  SerialIO_putb(data[1]);
+  SerialIO_puts(" - ");
+  SerialIO_putb(data[2]);
+  SerialIO_puts("\n");
+
+  EEPROM_write(addr, data, 3);
+
+  SerialIO_puts("Reading 3 bytes: ");
+  uint8_t *d = EEPROM_read(addr, 3);
+  SerialIO_putb(d[0]);
+  SerialIO_puts(" - ");
+  SerialIO_putb(d[1]);
+  SerialIO_puts(" - ");
+  SerialIO_putb(d[2]);
+  SerialIO_puts("\n");
+}
+
+void testIRLED(uint8_t input) {
+  SerialIO_puts("\n -- IR-LED TEST -- \n");
+  IRSend_send(0x8166817E, 32);
+  SerialIO_puts("AC should have toggled power\n");
+}
+
+void testDHT11(uint8_t input) {
+  SerialIO_puts("\n -- DHT-11 TEST -- \n");
+
+  uint8_t r = DHT11_readSensor();
+  SerialIO_puts("Read result: ");
+  SerialIO_putb(r);
+  SerialIO_puts(" Temp: ");
+  SerialIO_putb(DHT11_temperature());
+  SerialIO_puts("C Humidity: ");
+  SerialIO_putb(DHT11_humidity());
+  SerialIO_puts("%\n");
+}
+
+void testDIAGLEDS(uint8_t input) {
+  SerialIO_puts("\n -- DIAG LED TEST -- \n");
+
+  if (input == '1') {
+    RED_LED_High();
+    _delay_ms(100);
+    RED_LED_Low();
+  } else if (input == '2') {
+    YLW_LED_High();
+    _delay_ms(100);
+    YLW_LED_Low();
+  } else if (input == '3') {
+    GRN_LED_High();
+    _delay_ms(100);
+    GRN_LED_Low();
+  }
+}
+
+void mainTest(uint8_t input) {
+  testEEPROM(input);
+  testIRLED(input);
+  testDHT11(input);
+  testDIAGLEDS(input);
 }
---
+++ firmware/spoke/src/spoke.h
@@ -0,0 +1,7 @@
+#include <stdint.h>
+
+void testEEPROM(uint8_t input);
+void testIRLED(uint8_t input);
+void testDHT11(uint8_t input);
+void testDIAGLEDS(uint8_t input);
+void mainTest(uint8_t input);
