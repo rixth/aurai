bb4ba8bb6d98f721ec6ace546775bf99897732e7
Thomas Rix <tom@rixth.org>
2014-08-17 10:06:23 -0400

    Remove tests not used on spoke

--- firmware/spoke/src/spoke.cpp
+++ firmware/spoke/src/spoke.cpp
@@ -4,7 +4,6 @@
 #include <SPI.h>
 #include <DiagLEDS.h>
 #include <Serial.h>
-#include <EEPROM.h>
 #include <DHT11.h>
 #include <IRSend.h>
 #include <NRF24L01.h>
@@ -12,19 +11,11 @@
 #include <spoke.h>
 
 int main() {
+  // IRSend_init();
   Serial.begin();
-  EEPROM_init();
-  IRSend_init();
   SPI.begin();
   DiagLEDS_init();
   NRF24_init();
-
-  // Let components (esp. radio) wake up
-  _delay_ms(100);
-
-  RED_LED_ToOutput();
-  GRN_LED_ToOutput();
-  YLW_LED_ToOutput();
 
   while (1) {
     mainTest(Serial.read());
@@ -35,7 +26,6 @@ int main() {
 
 void mainTest(uint8_t input) {
   NRF24_test(input);
-  EEPROM_test(input);
   IRSend_test();
   DHT11_test();
   DiagLEDS_test(input);
